e1ddd7fa365bc354c6886698099674476fc1511a
Ciaran <mgqclseyn@163.com>
2019-04-12 09:43:05 +0800

    2019.4.12 9:42

--- webteam/src/app/app.module.ts
+++ webteam/src/app/app.module.ts
@@ -3,16 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ElModule } from 'element-angular'; 
+import { QuillModule } from 'ngx-quill';
+
+registerLocaleData(zh);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    ElModule.forRoot(),
+    BrowserAnimationsModule,
+    QuillModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
