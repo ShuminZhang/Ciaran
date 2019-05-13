c31d6e09c5dbbc785e1120a8be907bd6c322ca41
Ciaran <mgqclseyn@163.com>
2019-05-13 19:24:10 +0800

    2019.5.13  19:24

--- webteam/src/app/app-routing.module.ts
+++ webteam/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StartComponent } from './core/start/start.component';
+import { RegistComponent } from './core/regist/regist.component';
+import { LoginComponent } from './core/login/login.component';
+import { ForgetComponent } from './core/forget/forget.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'start',component:StartComponent},
+  {path:'login',component:LoginComponent},
+  {path:'regist',component:RegistComponent},
+  {path:'forget',component:ForgetComponent},
+  {path:'main',loadChildren:'./main/main.module#MainModule'},
+  {path:'**',redirectTo:'start'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
